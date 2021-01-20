3de6569cf8e25d3a6a0d0449737cb775e0b6d149
Wraith <i.acnaylor@gmail.com>
2021-01-20 12:36:39 +0000

    Comment Go BRRRRR

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Main from './components/main';
+import Main from './components/main'; // Importing the Main component to the main page
 
 function App() {
   return (
     <div className="App">
-    <Main />
+    <Main /> {/* Adding the Main component into the App */}
     </div>
   );
 }
 
-export default App;
+export default App; // I don't know where this goes tbh, think you just need to know up to here
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // Importing React Baseline and useState (allows you to do setColour... etc)
 
 const Main = () => {
 
-    // onMouseDown, onMouseEnter, onMouseLeave
+    // onMouseDown, onMouseEnter, onMouseLeave (Ignore this Nye)
 
     let colours = [
         'white',
@@ -13,16 +13,16 @@ const Main = () => {
         'purple', 
         'orange',
         'yellow'
-    ];
+    ]; // Declaring a list of colours
 
-    const [width, setWidth] = useState(20);
-    const [height, setHeight] = useState(20);
-    const [colour, setColour] = useState(colours[0])
+    const [width, setWidth] = useState(20); // Setting Width
+    const [height, setHeight] = useState(20); // Setting Height
+    const [colour, setColour] = useState(colours[0]) // Setting Colour
 
-    const cells = [];
+    const cells = []; // Creating a 2D Array for holding Rows
 
     for (let y = 0; y < height; y++) {
-        const row = [];
+        const row = []; // Creating an empty array
         for (let x = 0; x < width; x++) {
             row.push(
                 <div
@@ -30,30 +30,30 @@ const Main = () => {
                     onClick={(e) => {
                         e.target.style.background = colour;
                     }}    
-                ></div>
+                ></div> // Adding a "cell" div to the row array, and setting the background colour to the const colour, and changing the colour on click
             );
         }
-        cells.push(row);
+        cells.push(row); // Adding the row array to the cells array
     }
 
     const options = [
         colours.map(colour => <option value={ colour }> {colour} </option>)
-    ]
+    ] // Creating an <option> html tag array, with each colour being a new option (For a dropdown selection list)
     
     return (
         <>
-            <input type="number" min="1" value={ width } onChange={(e) => setWidth(e.target.value)}/> <br/>
-            <input type="number" min="1" value={ height } onChange={(e) => setHeight(e.target.value)} /> <br/>
+            <input type="number" min="1" value={ width } onChange={(e) => setWidth(e.target.value)}/> <br/>{/* Number selection box to set width */}            
+            <input type="number" min="1" value={ height } onChange={(e) => setHeight(e.target.value)} /> <br/>{/* Number selection box to set height */}
 
-            <select name="colour" id="colour" onChange={(e) => setColour(e.target.value)}>
-                { options }
+            <select name="colour" id="colour" onChange={(e) => setColour(e.target.value)}> {/* Creating a dropdown list of colours */}
+                { options } {/* Bringing in the colours to add to the list  */}
             </select>
 
-            { cells.map(row => <div className="row">{ row }</div>) }
+            { cells.map(row => <div className="row">{ row }</div>) } {/* Rendering the cells to the screen */}
         </>
-    );
+    ); // Returning a chunk of HTML to render
 
 };
 
 
-export default Main;
+export default Main; // Exporting the constant of Main 
